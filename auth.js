f536174a7f2e6799e4d031f3d1296a0e6aa6c44e
afvsf <145919614+afvsf@users.noreply.github.com>
2026-04-20 14:41:15 -0300

    Simplify access verification and add logout function
    Refactor access verification and logout functionality.

--- auth.js
+++ auth.js
@@ -1,23 +1,13 @@
-(function(){
+function verificarAcesso(){
 
   const token = localStorage.getItem('token');
 
   if(!token){
     window.location.href = 'login.html';
-    return;
   }
+}
 
-  try{
-    const payload = JSON.parse(atob(token.split('.')[1]));
-    const exp = payload.exp * 1000;
-
-    if(Date.now() > exp){
-      localStorage.removeItem('token');
-      window.location.href = 'login.html';
-    }
-
-  }catch{
-    window.location.href = 'login.html';
-  }
-
-})();
+function logout(){
+  localStorage.removeItem('token');
+  window.location.href = 'login.html';
+}
